d07d093220cab70598024d737da7f15af5d55c1d
Chad-Andrew Wolfe <ChadWolfie@gmail.com>
2019-04-30 16:36:11 -0400

    still working on the setup.sh

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#Setup
+
+echo "you must have python and pip installed..."
+
+echo
+echo "installing configparser"
+pip install configparser
+
+echo
+echo "installing flask"
+pip install flask
+
+echo
+echo "installing mysql-connector"
+pip install mysql-connector
+
+echo
+echo "creating config.ini"
